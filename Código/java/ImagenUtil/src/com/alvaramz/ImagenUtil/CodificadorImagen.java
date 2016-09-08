5108bcf5071d71d439e65d5fd19facbc73d0c42f
adrian.alvarado_r <adrian.alvarado_r@ucr.ac.cr>
2016-09-08 10:14:22 -0600

    Se agrega la funcionalidad para codificar una imagen en una hilera de caracteres utilizando la codificación Base64.

--- Código/java/ImagenUtil/src/com/alvaramz/ImagenUtil/CodificadorImagen.java
+++ Código/java/ImagenUtil/src/com/alvaramz/ImagenUtil/CodificadorImagen.java
@@ -1,10 +1,37 @@
 
 package com.alvaramz.ImagenUtil;
 
+import java.util.Base64;
+
 /**
- *
+ * Propociona métodos para codificar una imagen.
  * @author Ing. Adrián Alvarado Ramírez.
  */
 public class CodificadorImagen {
+    
+    /**
+     * Convierte una imagen almacenada en un arreglo de bytes a una hilera de
+     * caracteres en base64. Utiliza el método de codificacion provisto en el 
+     * API de java.
+     * @param imagen Contenido de la imagen, almacenado en un arreglo de bytes.
+     * @return String con la representación de la imagen en Base64.
+     */
+    public String imagenABase64(byte[] imagen){
+        return Base64.getEncoder().encodeToString(imagen);
+    }
+    
+    /**
+     * Convierte una imagen almacenada en un arreglo de bytes a una representación
+     * que se puede mostrar en una etiqueta html img. Para esto, codifica la imagen
+     * en Base64, utilizando el método imagenABase64.
+     * @param imagen
+     * @param extension
+     * @return 
+     */
+    public String imagenAHTMLData(byte[] imagen, String extension){
+       String imagenEnBase64 = imagenABase64(imagen);
+       String HTMLData =  String.format("data:image/%s;base64,%s", extension, imagenEnBase64);
+       return HTMLData;
+    }
     
 }
