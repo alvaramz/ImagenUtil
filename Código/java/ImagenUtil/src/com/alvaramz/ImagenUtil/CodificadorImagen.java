57d38c0d3c51eb513c4d5e38980b378828ba73f5
adrian.alvarado_r <adrian.alvarado_r@ucr.ac.cr>
2016-09-08 11:35:38 -0600

    Se modifica el método que hace la codificación, para que reciba el nombre completo del archivo, y éste se encarge de ejecutar la funcionalidad para extraer la extensión.

--- Código/java/ImagenUtil/src/com/alvaramz/ImagenUtil/CodificadorImagen.java
+++ Código/java/ImagenUtil/src/com/alvaramz/ImagenUtil/CodificadorImagen.java
@@ -24,14 +24,26 @@ public class CodificadorImagen {
      * Convierte una imagen almacenada en un arreglo de bytes a una representación
      * que se puede mostrar en una etiqueta html img. Para esto, codifica la imagen
      * en Base64, utilizando el método imagenABase64.
-     * @param imagen
-     * @param extension
+     * @param imagen Contenido de la imagen.
+     * @param nombre El nombre del archivo, se utiliza para extraer la extensión.
      * @return 
      */
-    public String imagenAHTMLData(byte[] imagen, String extension){
+    public String imagenAHTMLData(byte[] imagen, String nombre){
        String imagenEnBase64 = imagenABase64(imagen);
+       String extension = obtenerExtensionArchivo(nombre);
        String HTMLData =  String.format("data:image/%s;base64,%s", extension, imagenEnBase64);
        return HTMLData;
+    }
+    
+    /**
+     * Obtiene la extensión de un archivo a partir del nombre.
+     * @param nombre El nombre del archivo.
+     * @return 
+     */
+    private String obtenerExtensionArchivo(String nombre){
+        int indiceUltimoPunto = nombre.lastIndexOf('.');
+        String extension = nombre.substring(indiceUltimoPunto);
+        return extension;
     }
     
 }
